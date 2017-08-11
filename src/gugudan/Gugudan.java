e3eec1e3969c2cc59dc82841b345fd2064f289a6
Taehyeon Jake LEE <nayalth@gmail.com>
2017-08-11 18:03:27 +0900

    2, 3단 구현 - 계산 및 출

---
+++ src/gugudan/Gugudan.java
@@ -0,0 +1,28 @@
+package gugudan;
+
+public class Gugudan {
+    public static void main(String[] args) {
+        System.out.println("Hello CodeSquad");
+        
+        System.out.println("2 x 1 = " + (2 * 1));
+        System.out.println("2 x 2 = " + (2 * 2));
+        System.out.println("2 x 3 = " + (2 * 3));
+        System.out.println("2 x 4 = " + (2 * 4));
+        System.out.println("2 x 5 = " + (2 * 5));
+        System.out.println("2 x 6 = " + (2 * 6));
+        System.out.println("2 x 7 = " + (2 * 7));
+        System.out.println("2 x 8 = " + (2 * 8));
+        System.out.println("2 x 9 = " + (2 * 9));
+        
+        System.out.println("3 x 1 = " + (3 * 1));
+        System.out.println("3 x 2 = " + (3 * 2));
+        System.out.println("3 x 3 = " + (3 * 3));
+        System.out.println("3 x 4 = " + (3 * 4));
+        System.out.println("3 x 5 = " + (3 * 5));
+        System.out.println("3 x 6 = " + (3 * 6));
+        System.out.println("3 x 7 = " + (3 * 7));
+        System.out.println("3 x 8 = " + (3 * 8));
+        System.out.println("3 x 9 = " + (3 * 9));
+        
+    }
+}
