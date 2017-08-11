bc91422c898f856e00e92054266249209a0fc14d
Taehyeon Jake LEE <nayalth@gmail.com>
2017-08-11 19:01:53 +0900

    메소드를 활용해 구구단 다시 구

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -1,15 +1,28 @@
 package gugudan;
 
 public class Gugudan {
-    public static void main(String[] args) {
-        System.out.println("Hello CodeSquad");
+    
+    public static int[] calculate(int dan) {
 
         int[] result = new int[9];
         for (int i = 0; i < result.length; i++) {
-            result[i] = 2 * (i + 1);
+            result[i] = dan * (i + 1);
         }
+        return result;
+    }
+    
+    public static void print(int dan, int[] result) {
         for (int i = 0; i < result.length; i++) {
-            System.out.println("2 x " + (i + 1) + " = " + result[i]);
+            System.out.println(dan + " x " + (i + 1) + " = " + result[i]);
         }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello CodeSquad");
+        
+        for (int i = 2; i < 10; i++) {
+            print(i, calculate(i));
+        }
+        
     }
 }
