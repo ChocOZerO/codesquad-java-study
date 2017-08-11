2793664b74472538fe680c099ee1aa7a0bceb11c
Taehyeon Jake LEE <nayalth@gmail.com>
2017-08-11 18:35:12 +0900

    8, 9단 구현 - 값 입력 및 조건

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -1,52 +1,16 @@
 package gugudan;
 
-import java.util.Scanner;
-
 public class Gugudan {
     public static void main(String[] args) {
         System.out.println("Hello CodeSquad");
         
-        System.out.println("2 x 1 = " + (2 * 1));
-        System.out.println("2 x 2 = " + (2 * 2));
-        System.out.println("2 x 3 = " + (2 * 3));
-        System.out.println("2 x 4 = " + (2 * 4));
-        System.out.println("2 x 5 = " + (2 * 5));
-        System.out.println("2 x 6 = " + (2 * 6));
-        System.out.println("2 x 7 = " + (2 * 7));
-        System.out.println("2 x 8 = " + (2 * 8));
-        System.out.println("2 x 9 = " + (2 * 9));
-        
-        System.out.println("3 x 1 = " + (3 * 1));
-        System.out.println("3 x 2 = " + (3 * 2));
-        System.out.println("3 x 3 = " + (3 * 3));
-        System.out.println("3 x 4 = " + (3 * 4));
-        System.out.println("3 x 5 = " + (3 * 5));
-        System.out.println("3 x 6 = " + (3 * 6));
-        System.out.println("3 x 7 = " + (3 * 7));
-        System.out.println("3 x 8 = " + (3 * 8));
-        System.out.println("3 x 9 = " + (3 * 9));
-        
-        System.out.println("구구단 중 출력할 단은? : ");
-        Scanner sc = new Scanner(System.in);
-        int dan = sc.nextInt();
-        int result = dan * 1;
-        System.out.println(dan + " x 1 = " + result);
-        result = dan * 2;
-        System.out.println(dan + " x 2 = " + result);
-        result = dan * 3;
-        System.out.println(dan + " x 3 = " + result);
-        result = dan * 4;
-        System.out.println(dan + " x 4 = " + result);
-        result = dan * 5;
-        System.out.println(dan + " x 5 = " + result);
-        result = dan * 6;
-        System.out.println(dan + " x 6 = " + result);
-        result = dan * 7;
-        System.out.println(dan + " x 7 = " + result);
-        result = dan * 8;
-        System.out.println(dan + " x 8 = " + result);
-        result = dan * 9;
-        System.out.println(dan + " x 9 = " + result);
+        int i = 2;
+        while (i < 10) {
+            for (int j = 1; j < 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            i++;
+        }
         
     }
 }
