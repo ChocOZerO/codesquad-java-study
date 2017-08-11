80cab1a62948ad424a1e6de08d5ab8ea1acf991d
Taehyeon Jake LEE <nayalth@gmail.com>
2017-08-11 18:19:11 +0900

    4, 5단 구현 - 값 입력 변

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -1,5 +1,7 @@
 package gugudan;
 
+import java.util.Scanner;
+
 public class Gugudan {
     public static void main(String[] args) {
         System.out.println("Hello CodeSquad");
@@ -23,6 +25,28 @@ public class Gugudan {
         System.out.println("3 x 7 = " + (3 * 7));
         System.out.println("3 x 8 = " + (3 * 8));
         System.out.println("3 x 9 = " + (3 * 9));
+        
+        System.out.println("구구단 중 출력할 단은? : ");
+        Scanner sc = new Scanner(System.in);
+        int dan = sc.nextInt();
+        int result = dan * 1;
+        System.out.println(dan + " x 1 = " + result);
+        result = dan * 2;
+        System.out.println(dan + " x 2 = " + result);
+        result = dan * 3;
+        System.out.println(dan + " x 3 = " + result);
+        result = dan * 4;
+        System.out.println(dan + " x 4 = " + result);
+        result = dan * 5;
+        System.out.println(dan + " x 5 = " + result);
+        result = dan * 6;
+        System.out.println(dan + " x 6 = " + result);
+        result = dan * 7;
+        System.out.println(dan + " x 7 = " + result);
+        result = dan * 8;
+        System.out.println(dan + " x 8 = " + result);
+        result = dan * 9;
+        System.out.println(dan + " x 9 = " + result);
         
     }
 }
