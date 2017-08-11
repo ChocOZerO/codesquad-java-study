f58707abb810d1542c21be07dc23e84c3273a1f7
Taehyeon Jake LEE <nayalth@gmail.com>
2017-08-11 20:01:47 +0900

    클래스 활용하여 사용자가 입력한 값에 따라 크기가 다른 구구단을 계산해 출력

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -6,11 +6,12 @@ public class Gugudan {
     
     public static ArrayList<Integer> calculate(int dan) {
         ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 1; i < 10; i++) {
-            // 계산 결과를 ArrayList에 저장
-            result.add(dan * i);
+        for (int i = 2; i <= dan; i++) {
+            for (int j = 1; j <= dan; j++) {
+             // 계산 결과를 ArrayList에 저장
+                result.add(i * j);
+            }
         }
         return result;
     }
-    
 }
--- src/gugudan/GugudanMain.java
+++ src/gugudan/GugudanMain.java
@@ -1,25 +1,18 @@
 package gugudan;
 
-import java.util.Scanner;
-
 public class GugudanMain {
 
     public static void main(String[] args) {
         System.out.println("Hello CodeSquad");
         
         /* 
-        사용자가 숫자 값 하나를 입력하면 입력한 숫자의 구구단 결과를 출력한다.
+        사용자가 입력한 값에 따라 크기가 다른 구구단을 계산해 출력한다. 예를 들어 사용자가 8을 입력하면 팔팔단, 19를 입력하면 십구십구단(2 * 1에서 19 * 19)을 계산해 출력한다.
         위 요구사항 구현을 다음과 같이 클래스를 분리해 구현한다.
         
         사용자의 입력을 받는 클래스 - GugudanInput
         사용자 입력 값에 따른 구구단을 실행하는 클래스 - Gugudan
         구구단 실행 결과를 출력하는 클래스 - GugudanOutput
         이 3개의 클래스를 조합해 프로그램을 실행하는 클래스 - GuguandanMain
-        
-        GugudanInput은 int input() 메소드를 구현해야 한다.
-        Gugudan은 ArrayList<Integer> calculate(int times) 메소드를 구현해야 한다.
-        GugudanOutput은 void print(ArrayList<Integer> result) 메소드를 구현해야 한다.
-        GuguandanMain은 main() 메소드를 가지면 위 3개의 메소드를 조합해야 한다.
         */
         
         GugudanOutput.print(Gugudan.calculate(GugudanInput.input()));
